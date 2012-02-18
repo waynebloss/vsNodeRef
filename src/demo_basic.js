cc31b8ac2df62e39e005f2c204bd21d735207f95
Wayne Bloss <waynebloss@gmail.com>
2012-02-17 21:40:17 -0500

    Replaced demo_simple.js with demo_basic.js

---
+++ src/demo_basic.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="ref/node-v0.6.js"/>
+
+// NOTE: Reference xml must appear before anything else.
+//
+// The primary Node.js reference file (node-v0.6.js) will contain Intellisense
+// meta-data for all Node.js globals and all Node.js built-in modules. Edit this
+// file with any edition of Visual Studio 2008/2010 to see the result.
+//
+// Available Node.js Globals:
+//
+// - Buffer, global, process, require
+//
+// Available Node.js Built-In Modules:
+//
+// - events, util, stream, assert, buffer, timers, net
+// 
+var util = require('util');
+util.log('Hello, World? Are you there? It\'s me, Node.js.');
+
